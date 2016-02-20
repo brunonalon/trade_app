0f922b6acf3338db555f364e352053545c05252c
brunonalon <brunonalon2@gmail.com>
2016-02-20 00:46:20 +0000

    Add relations on models

--- app/app/models/location.rb
+++ app/app/models/location.rb
@@ -1,2 +1,4 @@
 class Location < ActiveRecord::Base
+  has_one :user
+  belongs_to :community
 end
--- app/app/models/user.rb
+++ app/app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ActiveRecord::Base
+  has_many :items
+  # has_many :messages, foreign_key: "user_sender_id", class_name: "Message"
+  # has_many :messages, foreign_key: "user_receiver_id", class_name: "Message"
+  has_many :sender, foreign_key: 'user_sender_id', :class_name => 'Message'
+  has_many :user_receiver_id, :class_name => 'Message'
+  belongs_to :location
 end
