4dbbba2618a691cd9b854e66934976ed62e6c234
brunonalon <brunonalon2@gmail.com>
2016-02-25 17:54:58 +0000

    Merge with item-feature

--- app/app/models/item.rb
+++ app/app/models/item.rb
@@ -1,9 +1,37 @@
-class Item < ActiveRecord::Base
+# class Item < ActiveRecord::Base
+#
+#   belongs_to :user, dependent: :destroy
+#   has_many :likes, dependent: :destroy
+#   has_many :dislikes, dependent: :destroy
+#   has_many :matches, dependent: :destroy
+#   mount_uploader :picture_url, ImageUploader
+#
+# end
 
-  belongs_to :user, dependent: :destroy
-  has_many :likes, dependent: :destroy
-  has_many :dislikes, dependent: :destroy
-  has_many :matches, dependent: :destroy
+
+class Item < ActiveRecord::Base
+  belongs_to :user
+  has_many :offered, foreign_key: 'item_offered_id', :class_name => 'Like'
+  has_many :like, foreign_key: 'item_like_id', :class_name => 'Like'
+
   mount_uploader :picture_url, ImageUploader
+
+  def get_match_items
+    match_items = []
+    likes = self.offered.all
+    likes.each {|like|
+      item_liked = like.like
+      match_items << item_liked unless item_liked.offered.where('item_like_id = ?', self.id).empty?
+    }
+    match_items
+  end
+
+  def get_all_items_that_like_me
+    items = []
+    self.like.each{|like|
+      items << like.offered
+    }
+    items
+  end
 
 end
--- app/app/models/like.rb
+++ app/app/models/like.rb
@@ -1,12 +1,19 @@
-class Like < ActiveRecord::Base
-  belongs_to :item_offered, :class_name => 'Item'
-  belongs_to :item_liked, :class_name => 'Item'
-  after_save :match?
+# class Like < ActiveRecord::Base
+#   belongs_to :item_offered, :class_name => 'Item'
+#   belongs_to :item_liked, :class_name => 'Item'
+#   after_save :match?
+#
+#   def match?
+#     #search likes for inverse of item ids
+#     result = Like.where(:item_offered=>self.item_liked).where(:item_liked=>self.item_offered)
+#     #if results > 0, then insert a match in the matches table
+#     Match.create(item_offered: self.item_offered, item_liked: self.item_liked) if result.length > 0
+#   end
+# end
 
-  def match?
-    #search likes for inverse of item ids
-    result = Like.where(:item_offered=>self.item_liked).where(:item_liked=>self.item_offered)
-    #if results > 0, then insert a match in the matches table
-    Match.create(item_offered: self.item_offered, item_liked: self.item_liked) if result.length > 0
-  end
+class Like < ActiveRecord::Base
+
+  belongs_to :offered, foreign_key: 'item_offered_id', :class_name => 'Item'
+  belongs_to :like, foreign_key: 'item_like_id', :class_name => 'Item'
+
 end
