853a6dced262eeade5a6e92089a3650df09f5a54
BlakSwan <chris.r.jackson78@gmail.com>
2016-02-27 13:15:49 -0700

    added match route
    added match route and start of match page

--- app/config/routes.rb
+++ app/config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
   root'pages#home'
   get 'items/index'
   get 'swipe/findstuff'
+  get 'matches/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
